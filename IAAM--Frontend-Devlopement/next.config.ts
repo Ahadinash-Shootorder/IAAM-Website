24fccbea54eff31598f2f4e0f60d3617a5998036
Ahadinash-Shootorder <ahadinash@shootorder.in>
2026-04-17 12:32:38 +0530

    fix standalone config

--- IAAM--Frontend-Devlopement/next.config.ts
+++ IAAM--Frontend-Devlopement/next.config.ts
@@ -2,8 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
+
+  output: "standalone",   // 👈 ADD THIS
+
   images: {
-    unoptimized: true,   
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
